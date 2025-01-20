e376670cee71c1b6a97f67fad40f77e13e248c98
Pranav Sai <pranavxd142@gmail.com>
2025-01-20 08:35:16 -0800

    Add print statement

--- GetRunup.py
+++ GetRunup.py
@@ -13,6 +13,7 @@ class GetRunup:
         WAVE_MWP_DATA_FILE="",
         ADCIRC_MESH_DATA_FILE="",
         RUNUP_DATA_FILE=""):
+        print("Generating Runup!", flush=True)
         temp_directory = RUNUP_DATA_FILE[0:RUNUP_DATA_FILE.rfind("/") + 1]
         with open(STATIONS_FILE) as stations_file:
             stationsDict = json.load(stations_file)
