723b4b20d206d8dab6c0e608d510b3449f6f6026
Pranav Sai <pranavxd142@gmail.com>
2025-01-26 18:26:13 -0800

    Add deepwater wavelength

--- GetRunup.py
+++ GetRunup.py
@@ -62,14 +62,19 @@ class GetRunup:
             offshoreMwp = mwpDict[offshoreKey]["mwp"]
             shorelineElevation = float(meshDict[key]["elevation"])
             offshoreElevation = float(meshDict[offshoreKey]["elevation"])
-            print(offshoreWater, offshoreSwh, offshoreMwd, offshoreMwp, shorelineElevation, offshoreElevation)
-            print("max time, water, swg, mwd, mwp, and elevation shoreline offshore", max(offshoreWater), max(offshoreSwh), max(offshoreMwd), max(offshoreMwp), shorelineElevation, offshoreElevation)
+#             print(offshoreWater, offshoreSwh, offshoreMwd, offshoreMwp, shorelineElevation, offshoreElevation)
+#             print("max time, water, swg, mwd, mwp, and elevation shoreline offshore", max(offshoreWater), max(offshoreSwh), max(offshoreMwd), max(offshoreMwp), shorelineElevation, offshoreElevation)
     #                             distance and threshold in kilometers
             distance = haversine.haversine(offshoreCoordinates, shorelineCoordinates) * 1000
             print("distance between offshore and shoreline", distance)
 #             Calculate average slope in radians
             averageSlope = math.atan((offshoreElevation - shorelineElevation) / distance)
             print("average slope", averageSlope)
+            
+            f = 9.81
+#             Convert mean wave period to deepwater wavelength
+            offshoreWavelength = (g * offshoreMwp**2) / (2 * math.pi)
+            print("offshoreWavelength", offshoreWavelength)
 #             First, calculate offshore node index from given runup station location (Can be hardcoded to a specific v18 node index)
 #               A way to find the shoreline and offshore point elevation, water level, and wave parameters,
 #               Observational stations can be set for the shoreline and offshore point. Then the values will be interpolated onto the points as
