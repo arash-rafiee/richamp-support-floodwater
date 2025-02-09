f62f9ba878f33807ea5fe6d6e8c13808d2396f68
Pranav Sai <pranavxd142@gmail.com>
2025-02-09 14:39:45 -0600

    Update with waves for dec23 command

--- localGenerator.sh
+++ localGenerator.sh
@@ -110,17 +110,25 @@
 # --generateRunup true \
 # --tempDir /scratch3/workspace/pranav_sai_uri_edu-manrun/temp/ --backgroundChoice CHARLESTOWN
 
-python generateGraphs.py --stations RUNUP_OFFSHORE_STATIONS.json \
---wavesExists true \
---waverad /Volumes/ssd/ObservationalWind/wave_data/rads.64.nc \
---waveswh /Volumes/ssd/ObservationalWind/wave_data/swan_HS.63.nc \
---wavemwd /Volumes/ssd/ObservationalWind/wave_data/swan_DIR.63.nc \
---wavemwp /Volumes/ssd/ObservationalWind/wave_data/swan_TMM10.63.nc \
---wavepwp /Volumes/ssd/ObservationalWind/wave_data/swan_TPS.63.nc \
---meshExists true --mesh /Volumes/ssd/ObservationalWind/wave_data/fort.14 \
---waterExists true --water /Volumes/ssd/ObservationalWind/wave_data/fort.63.nc \
---generateRunup true \
---tempDir /Volumes/ssd/temp/ --backgroundChoice CHARLESTOWN
+# python generateGraphs.py --stations RUNUP_OFFSHORE_STATIONS.json \
+# --wavesExists true \
+# --waverad /Volumes/ssd/ObservationalWind/wave_data/rads.64.nc \
+# --waveswh /Volumes/ssd/ObservationalWind/wave_data/swan_HS.63.nc \
+# --wavemwd /Volumes/ssd/ObservationalWind/wave_data/swan_DIR.63.nc \
+# --wavemwp /Volumes/ssd/ObservationalWind/wave_data/swan_TMM10.63.nc \
+# --wavepwp /Volumes/ssd/ObservationalWind/wave_data/swan_TPS.63.nc \
+# --meshExists true --mesh /Volumes/ssd/ObservationalWind/wave_data/fort.14 \
+# --waterExists true --water /Volumes/ssd/ObservationalWind/wave_data/fort.63.nc \
+# --generateRunup true \
+# --tempDir /Volumes/ssd/temp/ --backgroundChoice CHARLESTOWN
+
+
+# Water dec 23
+# python generateGraphs.py --stations OBS_STATIONS.json --obsExists true --waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-manrun/Dec172023RunupRun/forecast_RI_track/fort.63.nc --backgroundChoice RHODE_ISLAND_CHAMP --tempDir /scratch3/workspace/pranav_sai_uri_edu-manrun/temp/
+
+# Waves for dec23
+python generateGraphs.py --stations OBS_STATIONS.json --obsExists true --wavesExists true --waveswh /scratch3/workspace/pranav_sai_uri_edu-manrun/Dec172023RunupRun/forecast_RI_track/swan_HS.63.nc --wavemwd /scratch3/workspace/pranav_sai_uri_edu-manrun/Dec172023RunupRun/forecast_RI_track/swan_DIR.63.nc --wavemwp /scratch3/workspace/pranav_sai_uri_edu-manrun/Dec172023RunupRun/forecast_RI_track/swan_TMM10.63.nc --wavepwp /scratch3/workspace/pranav_sai_uri_edu-manrun/Dec172023RunupRun/forecast_RI_track/swan_TPS.63.nc --waverad /scratch3/workspace/pranav_sai_uri_edu-manrun/Dec172023RunupRun/forecast_RI_track/rads.64.nc --backgroundChoice RHODE_ISLAND_CHAMP --tempDir /scratch3/workspace/pranav_sai_uri_edu-manrun/temp/
+
 # python generateGraphs.py --stations OBS_STATIONS.json --meshExists true --mesh ../trackfiles/v18.fort.14 --tempDir temp/ --backgroundChoice CAPE_COD
 
 # python generateGraphs.py --stations OBS_STATIONS.json --obsExists true --gfsExists true --wind /Volumes/ssd/ObservationalWind/feb25.nc --tempDir /Volumes/ssd/temp/ --backgroundChoice EAST_COAST
