3b37c90aafa9eb90fb3848bbca558ad5f090200f
Pranav Sai <pranavxd142@gmail.com>
2025-03-22 18:39:32 -0500

    Generate runup at 5.5 km away

--- generateNormalPoints.py
+++ generateNormalPoints.py
@@ -13,7 +13,8 @@ HYPERPOINTS = 120
 # DEEPLINE_DISTANCE = 2400
 # 40m depthx`
 # DEEPLINE_DISTANCE = 3500
-DEEPLINE_DISTANCE = 4500
+# DEEPLINE_DISTANCE = 4500
+DEEPLINE_DISTANCE = 5500
 # offshore key depth -35.5
 
 def calculate_bearing(lat1, lon1, lat2, lon2):
